3c373e1b9496cbb7c5df30079c1fb9d25bf33096
janes-app <vividayu322@gmail.com>
2026-07-12 18:44:35 +0800

    fix backspace after calculation

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let currentInput="";
 function press(value){
+   if(currentInput==="0"){
+    currentInput=value;
+   }else{
     currentInput+=value;
+   }
     document.getElementById("display").innerHTML=currentInput;
 }
 function calculate(){
     let result=eval(currentInput);
     document.getElementById("display").innerHTML=result;
-    currentInput=result;
+    currentInput=result.toString();
 }
 function clearDisplay(){
     currentInput="";
